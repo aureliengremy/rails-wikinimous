ae790b342ed4c99b9e3737a84e1a974596bc5bd5
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-10 16:47:02 -0400

    Wikinimous-app working : with db and style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times {
+   article = Article.new({ title: Faker::Hacker.adjective, content: Faker::Hacker.say_something_smart })
+   article.save
+ }
